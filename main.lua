0885f55501bcde3875be9b5fcc4bea87643e30dc
crubz11 <max@kaiser.family>
2024-10-20 08:46:35 +0200

    added Collision Classes

---
+++ main.lua
@@ -0,0 +1,48 @@
+local love = require("love")
+local wf = require("libraries/windfield/windfield")
+
+function love.load()
+	world = wf.newWorld(0, 800, false)
+
+	world:addCollisionClass("Platform")
+	world:addCollisionClass("Player"--[[, { ignores = { "Platform" } }]])
+	world:addCollisionClass("Danger")
+
+	player = world:newRectangleCollider(360, 100, 80, 80, { collision_class = "Player" })
+	player:setFixedRotation(true)
+	player.speed = 240
+
+	platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = "Platform" })
+	platform:setType("static")
+
+	dangerZone = world:newRectangleCollider(0, 550, 800, 50, { collision_class = "Danger" })
+	dangerZone:setType("static")
+end
+
+function love.update(dt)
+	world:update(dt)
+
+	if player.body then
+		local px, py = player:getPosition()
+		if love.keyboard.isDown("d") then
+			player:setX(px + player.speed * dt)
+		end
+		if love.keyboard.isDown("a") then
+			player:setX(px - player.speed * dt)
+		end
+
+		if player:enter("Danger") then
+			player:destroy()
+		end
+	end
+end
+
+function love.draw()
+	world:draw()
+end
+
+function love.keypressed(key)
+	if key == "space" then
+		player:applyLinearImpulse(0, -7000)
+	end
+end
